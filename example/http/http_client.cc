869035fa173f6c8fa2e8b44e90f072fb1430fd4e
suweimin <swm8023@gmail.com>
2019-04-01 23:59:05 +0800

    修改http client qps计算方法

--- example/http/http_client.cc
+++ example/http/http_client.cc
@@ -7,48 +7,68 @@
 #include <ev++.h>
 #include <chrono>
 #include <vector>
+#include <atomic>
 
 using namespace v;
 using namespace v::io;
 using namespace v::co;
 
 
-void HttpClient(std::string ip, uint16_t port, int conn_num, int req_num) {
+static int g_thread_num = 2;
+static int g_conn_num = 10;
+static int g_req_num = 1000000;
+static std::string g_ip = "127.0.0.1";
+static uint16_t g_port = 8383;
+
+static std::atomic<int> g_remain_req = 0;
+std::chrono::high_resolution_clock::time_point g_begin, g_end;
+
+void Report() {
+	static bool reported = false;
+	if (reported)
+		return;
+	reported = true;
+
+	g_end = std::chrono::high_resolution_clock::now();
+	auto dur = std::chrono::duration_cast<std::chrono::milliseconds>(g_end - g_begin).count();
+
+	LogInfo("Total run ", dur / 1000.0, "s. request", g_req_num);
+	LogInfo("qps: ", g_req_num * 1.0 / dur * 1000.0);
+}
+
+void HttpClient() {
 	HttpRequest req;
 	req.Write(HTTP_GET, "/test");
 	req.SetHeader("Connection", "Keep-Alive");
 
-	int n_req = req_num;
-	int n_conn = conn_num;
-	for (int c = 0; c < conn_num; c++) {
+	for (int c = 0; c < g_conn_num; c++) {
 		RunCoroutine([&]() {
 			Connection<CodecHttpClient, Tcp> conn;
-			bool ret = conn.Connect(NetAddr{ ip, port });
-			if (ret) {
-				conn.Write(req);
-				conn.ReadUntilClose([&conn, &req, &n_req, &n_conn](HttpResponse const& resp) {
-					conn.Write(req);
-					if (--n_req <= 0) {
-						conn.Close();
-						if (--n_conn <= 0) {
-							ev::loop_ref loop = this_io::Get()->GetRawLoop();
-							loop.unloop();
-						}
-					}	
-				});
-			} else {
+			if (false == conn.Connect(NetAddr{ g_ip, g_port })) {
 				LogError("Connect failed");
-				conn.Close();
-				if (--n_conn <= 0) {
+				ev::loop_ref loop = this_io::Get()->GetRawLoop();
+				loop.unloop();
+				return;
+			}
+
+			conn.Write(req);
+			conn.ReadUntilClose([&](HttpResponse const& resp) {
+				--g_remain_req;
+				if (g_remain_req <= 0) {
+					Report();
 					ev::loop_ref loop = this_io::Get()->GetRawLoop();
 					loop.unloop();
+					return;
 				}
-			}
+				conn.Write(req);
+					
+			});
+			
 		});
 	}
 
 	this_io::Get()->Run();
-	LogInfo("thread end..... failed, finish", req_num - n_req);
+	// LogInfo("thread end..... ");
 }
 
 
@@ -61,34 +81,26 @@ int main(int argc, char *argv[]) {
 
 	Logger::Instance().SetLogLevel(LOG_LEVEL::LINFO);
 
-	int thread_num = 1;
-	int conn_num = 10;
-	int request_num = 100000;
-	std::string ip = "127.0.0.1";
-	uint16_t port = 8383;
+	if (argc > 1) g_thread_num = atoi(argv[1]);
+	if (argc > 2) g_conn_num = atoi(argv[2]);
+	if (argc > 3) g_req_num = atoi(argv[3]);
+	if (argc > 4) g_ip = std::string(argv[4]);
+	if (argc > 5) g_port = (uint16_t)atoi(argv[5]);
 
-	if (argc > 1) thread_num = atoi(argv[1]);
-	if (argc > 2) conn_num = atoi(argv[2]);
-	if (argc > 3) request_num = atoi(argv[3]);
-	if (argc > 4) ip = std::string(argv[4]);
-	if (argc > 5) port = (uint16_t)atoi(argv[5]);
+	g_remain_req = g_req_num;
 
-	LogInfo(ip, ":", port, " thread ", thread_num, "conn ", conn_num," request ", request_num);
-	std::vector<std::thread> t(thread_num);
+	LogInfo(g_ip, ":", g_port, " thread ", g_thread_num, ", conn ", g_conn_num,", request ", g_req_num);
+	std::vector<std::thread> t(g_thread_num);
 
-	std::chrono::high_resolution_clock::time_point t_begin = std::chrono::high_resolution_clock::now();
+	g_begin = std::chrono::high_resolution_clock::now();
 	auto now = std::chrono::system_clock::now();
-	for (int i = 0; i < thread_num; i++) {
-		t[i] = std::thread(std::bind(&HttpClient, ip, port, conn_num, request_num));
+	for (int i = 0; i < g_thread_num; i++) {
+		t[i] = std::thread(HttpClient);
 	}
-	for (int i = 0; i < thread_num; i++) {
+	for (int i = 0; i < g_thread_num; i++) {
 		t[i].join();
 	}
-	std::chrono::high_resolution_clock::time_point t_end = std::chrono::high_resolution_clock::now();
-	auto dur = std::chrono::duration_cast<std::chrono::milliseconds>(t_end - t_begin).count();
-
-	LogInfo("Total run ", dur /1000.0, "s. request", request_num * thread_num);
-	LogInfo("qps: ", request_num * thread_num * 1.0 / dur * 1000.0);
+	
 
 	getchar();
 
